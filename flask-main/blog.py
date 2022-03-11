c72a4d269008c962039409287658015e8ec08373
Eric Phillips <eap5662@psu.edu>
2022-03-10 22:46:49 -0500

    added 'blog.py'

---
+++ flask-main/blog.py
@@ -0,0 +1,19 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from . import auth
+from . import db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    database = db.get_db()
+    posts = database.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        'FROM post p JOIN user u on p.author_id = u.id'
+        'ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
