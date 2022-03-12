aad72fde5e4eeaed1fe797be328db2c82871186e
Eric Phillips <eap5662@psu.edu>
2022-03-12 01:11:26 -0500

    added update view and 'update.html'

--- flask-main/blog.py
+++ flask-main/blog.py
@@ -44,4 +44,48 @@ def create():
 
     return render_template('blog/create.html')
 
+def get_post(id, check_author=True):
+    database = db.get_db()
+    post = database.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post ID {id} doesn't exist")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int: id>/update', methods=('GET', 'POST'))
+@auth.login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = "Title is required"
+
+        if error is not None:
+            flash(error)
+
+        else:
+            database = db.get_db()
+            database.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            database.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html', post=post)
+
 
